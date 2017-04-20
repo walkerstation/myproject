df2f82006a222d2d300be8e928390b4c4ad18a00
root <root@ubuntu.(none)>
2017-04-19 19:46:48 -0700

    add user for test

--- uart.c
+++ uart.c
@@ -1,3 +1,5 @@
+/*user for test*/
+
 #define Base_address	0x13830000
 #define GPL2CON         (*(volatile unsigned long *) 0x11000100)
 #define GPL2DAT         (*(volatile unsigned long *) 0x11000104)
